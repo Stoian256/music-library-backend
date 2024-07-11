f74b877c4defb0ead466f35ba2ae1df91566edb1
Silviu Stoian <silviu_stoian25@yahoo.com>
2024-07-11 15:11:06 +0300

    Initial project setup Spring Bootapplication with SQLite configuration

---
+++ src/main/java/com/example/MusicLibraryBackend/MusicLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicLibraryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MusicLibraryBackend/MusicLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicLibraryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
